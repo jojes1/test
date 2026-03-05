313ea3d097b8d1fa67434326c72aa0dbbf864670
Jesper Jönsson <jeppej338@gmail.com>
2026-03-05 19:57:45 +0100

    neotree example

--- tmp.lua
+++ tmp.lua
@@ -1,26 +1,58 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("lualine").setup {
-        options = {
-          globalstatus = true,
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          ignore_focus = { "neo-tree" },
-          -- theme = custom_wombat,
-          theme = 'nordic',
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    {
+      "s1n7ax/nvim-window-picker",
+      version = "2.*",
+      opts = {
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { "terminal", "quickfix" },
+          },
         },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          -- lualine_c = { "diagnostics" },
-          lualine_c = { { "filename", path = 2 }, "diagnostics" },
-          lualine_x = { "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = {},
+      },
+    },
+  },
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+    -- use_default_mappings = false,
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = false },
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        hide_dotfiles = true,
+      },
+      window = {
+        mappings = {
+          ["P"] = "focus_preview",
         },
-        extensions = { "quickfix", "man", "fugitive" },
-      }
-    end
+      },
+    },
+    window = {
+      mappings = {
+        ["l"] = "open_with_window_picker",
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
 }
