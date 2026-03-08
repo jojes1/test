3cc132b5267c539ea014173b3ffc33f407aa6623
Jesper Jönsson <jeppej338@gmail.com>
2026-03-08 18:49:51 +0100

    Update

--- tmp.lua
+++ tmp.lua
@@ -1,143 +1,111 @@
-return {
-  "ibhagwan/fzf-lua",
-  commit = "047f6862bb0c9710c06e5432d7ad50da9ea00e46",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    {
-      "<leader>sw",
-      function()
-        require("fzf-lua").grep_cword()
-      end,
-      desc = "Word",
-    },
-    {
-      "<leader>sW",
-      function()
-        require("fzf-lua").grep_cWORD()
-      end,
-      desc = "WORD",
-    },
-    {
-      "<leader>sc",
-      function()
-        require("fzf-lua").lgrep_curbuf()
-      end,
-      desc = "Text current buffer",
-    },
-    {
-      "<leader>sh",
-      function()
-        require("fzf-lua").helptags()
-      end,
-      desc = "Help",
-    },
-    -- {
-    --   "<leader>so",
-    --   function()
-    --     require("fzf-lua").nvim_options()
-    --   end,
-    --   desc = "Options",
-    -- },
-    {
-      "<leader>sb",
-      function()
-        require("fzf-lua").buffers()
-      end,
-      desc = "Buffers",
-    },
-    {
-      "<leader>sgf",
-      function()
-        require("fzf-lua").git_files()
-      end,
-      desc = "Git files",
-    },
-    {
-      "<leader>sgs",
-      function()
-        require("fzf-lua").git_status()
-      end,
-      desc = "Git status",
-    },
-    {
-      "<leader>sgb",
-      function()
-        require("fzf-lua").git_branches()
-      end,
-      desc = "Git branches",
-    },
-    {
-      "<leader>sdd",
-      function()
-        require("fzf-lua").diagnostics_document()
-      end,
-      desc = "Current buffer",
-    },
-    {
-      "<leader>sdw",
-      function()
-        require("fzf-lua").diagnostics_workspace()
-      end,
-      desc = "Workspace",
-    },
-    { "<c-j>", "<c-j>", ft = "fzf", mode = "t", nowait = true },
-    { "<c-k>", "<c-k>", ft = "fzf", mode = "t", nowait = true },
-    { "<c-l>", "<c-l>", ft = "fzf", mode = "t", nowait = true },
-    { "f", "f", ft = "fzf", mode = "t", nowait = true },
-    -- { "//", LazyVim.pick("grep_visual"), mode = "v", desc = "Selection (Root Dir)" },
-    -- { "??", LazyVim.pick("grep_cword", { root = false }), desc = "Word (Root Dir)" },
-    -- { "??", LazyVim.pick("grep_visual", { root = false }), mode = "v", desc = "Selection (Root Dir)" },
-  },
-  opts = function(_, _) -- opts)
-    local config = require "fzf-lua.config"
-    -- Refer: https://github.com/ibhagwan/fzf-lua/blob/main/lua/fzf-lua/defaults.lua
-    local file_win_opts = {
-      fullscreen = true,
-      preview = {
-        layout = "vertical",
-        vertical = "up:70%",
-      },
-    }
-    config.defaults.actions.files["ctrl-t"] = require("fzf-lua.actions").file_tabedit
-    config.defaults.actions.files["ctrl-l"] = require("fzf-lua.actions").file_edit_or_qf
-    config.defaults.actions.files["alt-q"] = require("fzf-lua.actions").file_sel_to_qf
-    config.defaults.keymap.fzf["ctrl-q"] = "select-all+accept"
-    config.defaults.keymap.builtin["<a-f>"] = "toggle-fullscreen"
-    -- config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
-    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
-    return {
-      defaults = {
-        formatter = "path.filename_first",
-        header = false,
-        git_icons = false,
-        file_icons = false,
-        color_icons = false,
-        prompt = " > ",
-      },
-      fzf_opts = {
-        ["--cycle"] = true,
-        ["--prompt"] = " > ",
-        ["--no-scrollbar"] = true,
-      },
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
 
-      helptags = { winopts = file_win_opts },
-      files = {
-        rg_opts = [[--files -g "!*.toml" -g "!venv/" -g "!__pycache__/"]],
-        -- winopts = file_win_opts,
-        prompt = " > ",
-        cwd_prompt_shorten_len = 200,
-      },
-      grep = {
-        rg_opts = [[-g "!venv/" -g "!__pycache__/"]]
-      }
-      -- grep = { winopts = file_win_opts },
-      -- oldfiles = { winopts = file_win_opts },
-      -- git = {
-      --   files = {
-      --     winopts = file_win_opts,
-      --   },
-      -- },
-    }
-  end,
-}
+vim.keymap.set("n", "<leader>v", "<C-W>v", { desc = "Make verical split" })
+vim.keymap.set("n", "<leader>h", "<C-W>s", { desc = "Make horizontal split" })
 
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-W>h", { desc = "Jump to the left window when in terminal mode", noremap = true, silent = true })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-W>j", { desc = "Jump to the window below when in terminal mode", noremap = true, silent = true })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-W>k", { desc = "Jump to the window above when in terminal mode", noremap = true, silent = true })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-W>l", { desc = "Jump to the right window when in terminal mode", noremap = true, silent = true })
+vim.keymap.set("t", "ff", "<C-\\><C-n>", { desc = "Escape from terminal mode", noremap = true, silent = true })
+
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+-- vim.cmd "vnoremap <silent> y y`]"
+-- vim.cmd "vnoremap <silent> p p`]"
+-- vim.cmd "nnoremap <silent> p p`]"
+
+vim.keymap.set("n", "<Esc>", ":noh<CR>", { silent = true })
+-- vim.keymap.set("v", "p", '"_dP')
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle explorer" })
+
+vim.keymap.set(
+  "n",
+  "<Up>",
+  "[c",
+  { noremap = true, silent = true, desc = "Jump backwards to the previous start of a change" }
+)
+
+vim.keymap.set(
+  "n",
+  "<Down>",
+  "]c",
+  { noremap = true, silent = true, desc = "Jump forwards to the next start of a change" }
+)
+
+vim.keymap.set(
+  "n",
+  "<Right>",
+  "zR",
+  { noremap = true, silent = true, desc = "Open all folds.  This sets 'foldlevel' to highest fold level" }
+)
+
+vim.keymap.set(
+  "n",
+  "<Left>",
+  ":edit<cr>zM",
+  { noremap = true, silent = true, desc = "Close all folds: set 'foldlevel' to 0" }
+)
+
+local list_on = false
+vim.keymap.set("n", "<leader>ml", function()
+  if not list_on then
+    vim.cmd "set list"
+    list_on = true
+  else
+    vim.cmd "set nolist"
+    list_on = false
+  end
+end, { desc = "Toggle list" })
+
+-- vim.keymap.set("n", "<C-n>", function()
+--   require("fzf-lua").fzf_exec({ "Command", "Task" }, {
+--     prompt = "Function > ",
+--     actions = {
+--       ["ctrl-l"] = function(selected, opts)
+--         if selected[1] == "Command" then
+--           print("Command selection")
+--           -- command_selection()
+--         elseif selected[1] == "Task" then
+--           require("jesper.core.api.task-selection").task_selection()
+--         end
+--       end,
+--     },
+--   })
+-- end, { desc = "Select Function" })
+
+vim.keymap.set("n", "<C-p>", function()
+  -- local curwd = vim.fn.getcwd()
+  -- if curwd == "C:\\Users\\jeppe\\AppData\\Local\\nvim" then
+    -- require("fzf-lua").files { rg_opts = "--files -g '!*.toml' -g '!*.json' -g '!*.git/' -g '!*.gitignore'" }
+  -- elseif curwd == "/home/jesper/pnx/linux-applications/dpa-mx-printer-app" then
+  --   require("fzf-lua").files { rg_opts = "--files -g '*.cpp' -g '*.hpp' -g '*.py'" }
+  -- elseif curwd == "/home/jesper/pnx/tools/dpa-mx-phoenix-test" then
+  --   require("fzf-lua").files { rg_opts = "--files -g '*.py' -g '*.feature'" }
+  -- else
+    require("fzf-lua").files()
+  -- end
+end, { desc = "Find files" })
+
+vim.keymap.set("n", "<leader>st", function()
+  -- local curwd = vim.fn.getcwd()
+  -- if curwd == "C:\\Users\\jeppe\\AppData\\Local\\nvim" then
+  --   require("fzf-lua").live_grep { rg_opts = "--column -n -g '!*.toml' -g '!*.json' -g '!*.git/' -g '!*.gitignore'" }
+  -- elseif curwd == "/home/jesper/pnx/linux-applications/dpa-mx-printer-app" then
+  --   require("fzf-lua").live_grep { rg_opts = "--column -n -g '*.cpp' -g '*.hpp' -g '*.py'" }
+  -- elseif curwd == "/home/jesper/pnx/tools/dpa-mx-phoenix-test" then
+  --   require("fzf-lua").live_grep { rg_opts = "--column -n -g '*.py' -g '*.feature'" }
+  -- else
+    require("fzf-lua").live_grep()
+  -- end
+end, { desc = "Find text" })
