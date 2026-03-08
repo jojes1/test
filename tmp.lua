b7357faeba309a00171081c79cb8c833fff57c05
Jesper Jönsson <jeppej338@gmail.com>
2026-03-08 18:09:50 +0100

    Update

--- tmp.lua
+++ tmp.lua
@@ -1,58 +1,30 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    {
-      "s1n7ax/nvim-window-picker",
-      version = "2.*",
-      opts = {
-        filter_rules = {
-          include_current_win = false,
-          autoselect_one = true,
-          -- filter using buffer options
-          bo = {
-            -- if the file type is one of following, the window will be ignored
-            filetype = { "neo-tree", "neo-tree-popup", "notify" },
-            -- if the buffer type is one of following, the window will be ignored
-            buftype = { "terminal", "quickfix" },
-          },
-        },
+  "neogitorg/neogit",
+  event = "VeryLazy",
+  config = function()
+    local icons = require "jesper.core.icons"
+    vim.keymap.set("n", "<leader>gn", "<cmd>Neogit<CR>", { desc = "Neogit" })
+    require("neogit").setup {
+      auto_refresh = true,
+      disable_builtin_notifications = false,
+      use_magit_keybindings = false,
+      -- Change the default way of opening neogit
+      kind = "tab",
+      -- Change the default way of opening the commit popup
+      commit_popup = {
+        kind = "split",
       },
-    },
-  },
-  opts = {
-    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
-    -- use_default_mappings = false,
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = false },
-      use_libuv_file_watcher = true,
-      filtered_items = {
-        hide_dotfiles = true,
+      -- Change the default way of opening popups
+      popup = {
+        kind = "split",
       },
-      window = {
-        mappings = {
-          ["P"] = "focus_preview",
-        },
+      -- customize displayed signs
+      signs = {
+        -- { CLOSED, OPENED }
+        section = { icons.ui.ChevronRight, icons.ui.ChevronShortDown },
+        item = { icons.ui.ChevronRight, icons.ui.ChevronShortDown },
+        hunk = { "", "" },
       },
-    },
-    window = {
-      mappings = {
-        ["l"] = "open_with_window_picker",
-        ["<space>"] = "none",
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-    },
-  },
+    }
+  end,
 }
