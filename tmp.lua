1e4acfe152fd1f0ae1626a8d1f2599ecd0df4d69
Jesper Jönsson <jeppej338@gmail.com>
2026-04-05 19:07:31 +0200

    Update

--- tmp.lua
+++ tmp.lua
@@ -1,19 +1,128 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  commit = "fd59f984416f696d85119fd4d15ce0965b179944",
-  event = { "BufReadPost", "BufNewFile" },
-  lazy = false,
-  build = ":TSUpdate",
-
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "bash", "python", "c", "cpp" },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-emoji",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-buffer",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-path",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-cmdline",
+      event = "InsertEnter",
+    },
+    {
+      "saadparwaiz1/cmp_luasnip",
+      event = "InsertEnter",
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
       },
-      -- indent = { enable = false, disable = { "python" } },
-      indent = { enable = true },
+    },
+    {
+      "hrsh7th/cmp-nvim-lua",
+    },
+    {
+      "onsails/lspkind.nvim",
+    },
+  },
+  config = function()
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    local lspkind = require "lspkind"
+    require("luasnip/loaders/from_vscode").lazy_load()
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert {
+        ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-l>"] = cmp.mapping {
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        },
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-h>"] = cmp.mapping.confirm { select = true },
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
+      formatting = {
+        -- fields = { "kind", "abbr", "menu" },
+        format = lspkind.cmp_format {
+          maxwidth = 50,
+          ellipsis_char = "...",
+        },
+
+        expandable_indicator = true,
+      },
+
+      sources = {
+        -- { name = "copilot" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "cmp_tabnine" },
+        { name = "nvim_lua" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "calc" },
+        { name = "emoji" },
+        { name = "treesitter" },
+      },
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      window = {
+        completion = {
+          border = "rounded",
+          scrollbar = false,
+        },
+        documentation = {
+          border = "rounded",
+        },
+      },
+      experimental = {
+        ghost_text = false,
+      },
     }
   end,
 }
+
