b52782c342146459bbe48788cf3aaba737b1f630
Jesper Jönsson <jeppej338@gmail.com>
2026-05-19 06:53:55 +0200

    Update

--- tmp.lua
+++ tmp.lua
@@ -1,101 +1,56 @@
-vim.opt.number = true -- Line numbers
-vim.opt.cursorline = true -- Highlight current line
-vim.opt.wrap = false -- Don't wrap lines
-vim.opt.scrolloff = 10 -- Keep 10 lines above/below cursor
-vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+vim.keymap.set('i', 'jk', '<esc>', { desc = 'Exit insert mode' })
+vim.keymap.set('i', 'jj', '<esc>', { desc = 'Exit insert mode' })
 
--- Indentation
-vim.opt.tabstop = 2 -- Tab width
-vim.opt.shiftwidth = 2 -- Indent width
-vim.opt.softtabstop = 2 -- Soft tab stop
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.smartindent = true -- Smart auto-indenting
+-- Move between windows with Ctrl+hjkl (like tmux)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
--- Search settings
-vim.opt.ignorecase = true -- Case insensitive search
-vim.opt.smartcase = true -- Case sensitive if uppercase in search
-vim.opt.hlsearch = false -- Don't highlight search results
+-- Resize windows with Ctrl+Shift+arrows
+vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +5<cr>", { desc = "Resize up", noremap = true, silent = true } )
+vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -5<cr>", { desc = "Resize down", noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -5<cr>", { desc = "Resize left", noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +5<cr>", { desc = "Resize right", noremap = true, silent = true })
 
--- Visual settings
-vim.opt.termguicolors = true -- Enable 24-bit colors
-vim.opt.signcolumn = "yes" -- Always show sign column
-vim.opt.showmatch = true -- Highlight matching brackets
-vim.opt.matchtime = 2 -- How long to show matching bracket
-vim.opt.showmode = false -- Don't show mode in command line
-vim.opt.pumheight = 10 -- Popup menu height
-vim.opt.pumblend = 10 -- Popup menu transparency
-vim.opt.completeopt = "menu,menuone,noselect,fuzzy"
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.synmaxcol = 300 -- Syntax highlighting limit
-vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-vim.opt.winminwidth = 5 -- Minimum window width
+-- Clear search highlighting
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
--- File handling
-vim.opt.writebackup = false -- Don't create backup before writing
-vim.opt.swapfile = false -- Don't create swap files
-vim.opt.undofile = true -- Persistent undo
-vim.opt.updatetime = 300 -- Faster completion
-vim.opt.timeoutlen = 300 -- Lower than default (1000) to quickly trigger which-key
-vim.opt.ttimeoutlen = 0 -- Key code timeout
+-- Smart search navigation (n always goes forward, N always backward)
+vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
--- Behavior settings
-vim.opt.iskeyword:append("-") -- Treat dash as part of word
-vim.opt.path:append("**") -- include subdirectories in search
-vim.opt.mouse = "a" -- Enable mouse support
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+-- Better indenting (stay in visual mode)
+vim.keymap.set("v", "<S-Tab>", "<gv")
+vim.keymap.set("v", "<Tab>", ">gv")
 
--- Folding settings
-vim.opt.smoothscroll = true
-vim.wo.foldmethod = "expr"
-vim.opt.foldlevel = 99 -- Start with all folds open
-vim.opt.formatoptions = "jcroqlnt" -- tcqj
-vim.opt.grepformat = "%f:%l:%c:%m"
-vim.opt.grepprg = "rg --vimgrep"
+-- Save file (works in all modes)
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 
--- Split behavior
-vim.opt.splitbelow = true -- Horizontal splits go below
-vim.opt.splitright = true -- Vertical splits go right
-vim.opt.splitkeep = "screen"
+-- function PersistentSignatureHelp()
+--     vim.api.nvim_exec2([[
+--         augroup SigHelp
+--             autocmd!
+--             autocmd CursorMovedI <buffer> lua vim.lsp.buf.signature_help()
+--         augroup end
+--         autocmd InsertLeave <buffer> ++once augroup! SigHelp
+--     ]], {})
+--     vim.lsp.buf.signature_help()
+-- end
+-- vim.keymap.set('i', '<C-g>', '<cmd>lua PersistentSignatureHelp()<CR>', {})
 
--- Command-line completion
-vim.opt.wildmode = "longest:full,full"
-vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
+vim.keymap.set("i", "<M-g>", "<esc>lli")
+vim.keymap.set('i', '<C-g>', '(<bs>', {}) -- to get signature help after that the parantheces have been inserted after autocompletion
 
--- Better diff options
-vim.opt.diffopt:append("linematch:60")
+vim.keymap.set("n", "<C-p>", function()
+  require("fzf-lua").files({ fzf_opts = {['--layout'] = 'reverse-list' } })
+end, { desc = "Find files" })
 
--- Performance improvements
-vim.opt.maxmempattern = 20000
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Show diagnostic" })
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = "Goto definition" })
 
-vim.opt.fillchars = {
-  foldopen = "-",
-  foldclose = "+",
-  fold = " ",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ",
-  horiz = '━',
-  horizup = '┻',
-  horizdown = '┳',
-  vert = '┃',
-  vertleft = '┫',
-  vertright = '┣',
-  verthoriz = '╋',
-}
-
-vim.opt.jumpoptions = "view"
-vim.opt.laststatus = 3 -- global statusline
-vim.opt.linebreak = true -- Wrap lines at convenient points
-vim.opt.list = false -- Show some invisible characters (tabs...), is as default, but sometimes I may want this on
-vim.opt.shiftround = true -- Round indent
-vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
-
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.winborder = 'rounded'
-vim.opt.breakindent = true
-require('vim._core.ui2').enable()
-vim.cmd "packadd nvim.undotree"
-vim.cmd "packadd nvim.difftool"
+-- Dependency to plugins
