fbebb488624dae43090748a39a83c85fe862c23d
Jesper Jönsson <jeppej338@gmail.com>
2026-05-19 07:07:52 +0200

    Update

--- tmp.lua
+++ tmp.lua
@@ -1,55 +1,60 @@
-vim.lsp.enable({
-  "lua_ls",
-  "clangd",
-  "vim-language-server",
-})
+local ls = require "luasnip"
 
-vim.diagnostic.config({
-  -- virtual_lines = true,
-    virtual_text = false,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-      border = "rounded",
-      source = true,
-    },
-    signs = {
-      text = {
-        [vim.diagnostic.severity.ERROR] = "󰅚 ",
-        [vim.diagnostic.severity.WARN] = "󰀪 ",
-        [vim.diagnostic.severity.INFO] = "󰋽 ",
-        [vim.diagnostic.severity.HINT] = "󰌶 ",
-      },
-      numhl = {
-        [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-        [vim.diagnostic.severity.WARN] = "WarningMsg",
-      },
-    },
-    jump = {
-      -- When jumping, automatically open the floating window
-      on_jump = function(_, _)
-        vim.diagnostic.open_float({
-          -- Focus = false keeps cursor in the code, true jumps into the float
-          focus = false,
-          -- "cursor" shows only error at cursor, "line" shows all on line
-          scope = "cursor",
-          -- Optional: close float when moving cursor or switching buffers
-          close_events = {"BufLeave", "CursorMoved", "InsertEnter", "FocusLost"},
-        })
-      end,
-    },
+local s = ls.s
+local i = ls.i
+-- local t = ls.t
 
-    -- vim.api.nvim_create_autocmd('LspAttach', {
-    --   group = vim.api.nvim_create_augroup('my.lsp', {}),
-    --   callback = function(ev)
-    --     local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-    --
-    --     if client:supports_method('textDocument/completion') then
-    --       vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
-    --     end
-    --   end,
-    -- })
+-- local d = ls.dynamic_node
+-- local c = ls.choice_node
+-- local f = ls.function_node
+-- local sn = ls.snippet_node
 
-})
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
+local snippets, autosnippets = {}, {}
+
+-- local group = vim.api.nvim_create_augroup("Cpp Snippets", { clear = true })
+-- local file_pattern = "*.cpp | *.hpp"
+
+local cout_snippet = s(
+  "mycout",
+  fmt(
+    [[
+std::cout << {} << std::endl;{}
+  ]],
+    {
+      i(1),
+      i(0),
+    }
+  )
+)
+
+local incguard_snippet = s(
+  "myguard",
+  fmt(
+    [[
+#ifndef {}_{}_
+#define {}_{}_
+
+{}
+
+#endif /* {}_{}_ */
+
+  ]],
+    {
+      i(1),
+      i(2),
+      rep(1),
+      rep(2),
+      i(0),
+      rep(1),
+      rep(2)
+    }
+  )
+)
+
+table.insert(snippets, cout_snippet)
+table.insert(snippets, incguard_snippet)
+
+return snippets, autosnippets
